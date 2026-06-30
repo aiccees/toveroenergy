f7f359d637e580a82c54bc6f8ffb85a8d98587ea
Nathaniel Essien <thaniessy@gmail.com>
2026-06-30 08:29:27 +0100

    Implement sponsors marquee animation and refactor carousel component for improved layout and performance

--- src/components/sponsors_carousel/carousel.jsx
+++ src/components/sponsors_carousel/carousel.jsx
@@ -5,11 +5,6 @@ import trans from "@/public/images/home/trans.svg";
 import uniport from "@/public/images/home/unipor.svg";
 import inmotv from "@/public/images/home/inmotv.jpg";
 import tennLogo from "@/public/images/home/tenn-logo.png";
-
-// //// SWIPER IMPORTS /////// //
-import { Pagination, Scrollbar, Navigation, Autoplay } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css/bundle";
 import Image from "next/image";
 import { cn } from "@/utils/utils";
 
@@ -23,63 +18,32 @@ const sponsors_list = [
   { id: 7, image: tennLogo },
 ];
 
-function SponsorsCarousel() {
+function SponsorLogo({ item }) {
   return (
-    <Swiper
-      modules={[Autoplay]}
-      //   slidesPerView={3}
-      autoplay={{
-        delay: 2500,
-        // disableOnInteraction: false,
-      }}
-      breakpoints={{
-        320: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        480: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
+    <div className="flex shrink-0 items-center justify-center px-6 md:px-10">
+      <Image
+        src={item.image}
+        alt={`sponsor image ${item.id}`}
+        className={cn(
+          "h-10 w-auto object-contain md:h-14",
+          [6, 7].includes(item.id) && "h-14 md:h-20"
+        )}
+      />
+    </div>
+  );
+}
 
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 30,
-        },
+function SponsorsCarousel() {
+  const marqueeItems = [...sponsors_list, ...sponsors_list];
 
-        1024: {
-          slidesPerView: 5,
-          spaceBetween: 50,
-        },
-      }}
-
-    >
-      {sponsors_list.map((item) => (
-        <SwiperSlide
-          key={item.id}
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            // backgroundColor: "red"
-          }}
-        >
-          <Image
-            src={item.image}
-            alt={`sponsor image ${item.id}`}
-            className={cn(
-              "w-auto", 
-              [6, 7].includes(item.id) && "w-1/2"
-            )}
-       
-          />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+  return (
+    <div className="sponsors-marquee w-full overflow-hidden">
+      <div className="sponsors-marquee-track flex w-max items-center">
+        {marqueeItems.map((item, index) => (
+          <SponsorLogo key={`${item.id}-${index}`} item={item} />
+        ))}
+      </div>
+    </div>
   );
 }
 
